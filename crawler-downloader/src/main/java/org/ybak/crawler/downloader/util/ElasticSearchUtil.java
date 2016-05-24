b462ec9a7dc7fc74c109b7cfeafe8bb4a665be44
yu <bing.yu@dmall.com>
2016-05-24 18:47:19 +0800

    添加替代sql的匹配的 elasticsearch query dsl 语句.

--- crawler-downloader/src/main/java/org/ybak/crawler/downloader/util/ElasticSearchUtil.java
+++ crawler-downloader/src/main/java/org/ybak/crawler/downloader/util/ElasticSearchUtil.java
@@ -3,12 +3,17 @@ package org.ybak.crawler.downloader.util;
 import com.alibaba.fastjson.JSON;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.index.query.QueryBuilders;
+
+import static org.elasticsearch.index.query.QueryBuilders.*;
+
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.ybak.crawler.persistence.vo.Mail;
 
 import java.io.IOException;
@@ -33,14 +38,14 @@ public class ElasticSearchUtil {
     }
 
     public static void main(String[] args) throws Exception {
-        SearchResponse response = client.prepareSearch("twitter")
+        SearchRequestBuilder request = client.prepareSearch("chengdu12345")
                 .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                .setQuery(QueryBuilders.termQuery("content", "红牌楼"))                 // Query
-//                .setPostFilter(QueryBuilders.rangeQuery("age").from(12).to(18))     // Filter
-                .setFrom(0).setSize(60).setExplain(true)
-                .execute()
-                .actionGet();
-        System.out.println(JSON.toJSONString(response));
+                .setQuery(multiMatchQuery("红牌楼", "title", "content", "result").type(MatchQueryBuilder.Type.PHRASE)) // Query
+                .addSort("createDate", SortOrder.DESC)
+                .setFrom(0).setSize(60).setExplain(true);
+        System.out.println(request.toString());
+        SearchResponse response = request.execute().actionGet();
+        System.out.println(response.getHits().totalHits());
     }
 
     public static void indexMails(Iterable<Mail> mails) {
@@ -48,28 +53,26 @@ public class ElasticSearchUtil {
         for (Mail mail : mails) {
             addMailIndexRequest(bulkRequest, mail);
         }
-
         BulkResponse bulkResponse = bulkRequest.get();
-
         System.out.println(JSON.toJSONString(bulkResponse));
     }
 
     private static void addMailIndexRequest(BulkRequestBuilder bulkRequest, Mail mail) {
         try {
-            bulkRequest.add(client.prepareIndex("twitter", "tweet", ""+mail.id)
-                .setSource(jsonBuilder()
-                    .startObject()
-                    .field("content", mail.content)
-                    .field("createDate", mail.createDate)
-                    .field("acceptUnit", mail.acceptUnit)
-                    .field("category", mail.category)
-                    .field("result", mail.result)
-                    .field("sender", mail.sender)
-                    .field("status", mail.status)
-                    .field("title", mail.title)
-                    .field("url", mail.url)
-                    .field("views", mail.views)
-                .endObject())
+            bulkRequest.add(client.prepareIndex("chengdu12345", "mail", "" + mail.id)
+                            .setSource(jsonBuilder()
+                                    .startObject()
+                                    .field("content", mail.content)
+                                    .field("createDate", mail.createDate)
+                                    .field("acceptUnit", mail.acceptUnit)
+                                    .field("category", mail.category)
+                                    .field("result", mail.result)
+                                    .field("sender", mail.sender)
+                                    .field("status", mail.status)
+                                    .field("title", mail.title)
+                                    .field("url", mail.url)
+                                    .field("views", mail.views)
+                                    .endObject())
             );
         } catch (IOException e) {
             throw new RuntimeException(e);
