fe0baf38ec9be92d2c8c24015e387fc239717b58
yu <bing.yu@dmall.com>
2016-05-26 16:31:13 +0800

    保证web的正常运行.

--- crawler-search-web/src/main/java/org/ybak/crawler/web/WebApplication.java
+++ crawler-search-web/src/main/java/org/ybak/crawler/web/WebApplication.java
@@ -2,14 +2,13 @@ package org.ybak.crawler.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
 
 @SpringBootApplication(scanBasePackages = {
         "org.ybak.crawler.persistence.service",
         "org.ybak.crawler.web"
 })
 //@EnableJpaRepositories("org.ybak.crawler.persistence.repo")
-@EntityScan("org.ybak.crawler.persistence.vo")
+//@EntityScan("org.ybak.crawler.persistence.vo")
 public class WebApplication {
 
     public static void main(String[] args) throws Exception {
